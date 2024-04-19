a5ab43a61b07b3ba1438a851c6a0b465af9b663c
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-19 19:23:01 +0200

    Initial commit

---
+++ src/Tag01/HalloWelt.java
@@ -0,0 +1,12 @@
+package Tag01;
+
+public class HalloWelt {
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+
+        System.out.print("Hallo 1");
+        System.out.print("Hallo 2");
+
+        System.out.println("Hallo Welt!");
+    }
+}
