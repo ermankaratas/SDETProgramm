ec6fab1da365845bc69d942bb45340a3b6e69787
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-19 19:28:39 +0200

    \n verwendet.

--- src/Tag01/HalloWelt.java
+++ src/Tag01/HalloWelt.java
@@ -8,5 +8,16 @@ public class HalloWelt {
         System.out.print("Hallo 2");
 
         System.out.println("Hallo Welt!");
+
+        System.out.println("D\ni\ne\nn\ns\nt\na\ng");
+
+        System.out.println("M\no\nn\nt\na\ng");
+        System.out.println("------");
+        System.out.println("M");
+        System.out.println("o");
+        System.out.println("n");
+        System.out.println("t");
+        System.out.println("a");
+        System.out.println("g");
     }
 }
