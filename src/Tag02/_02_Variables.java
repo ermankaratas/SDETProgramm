1e08f53577f43f1fbfc25d07a77ccbb0124005a8
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-20 20:46:42 +0200

    Variables

---
+++ src/Tag02/_02_Variables.java
@@ -0,0 +1,17 @@
+package Tag02;
+
+public class _02_Variables {
+    public static void main(String[] args) {
+        int zahl;
+        zahl = 5;
+        System.out.println("zahl");
+        System.out.println(zahl);
+        System.out.println("Zahl=" + zahl);
+
+        int seite = 5;
+        int umfang = seite + seite + seite + seite;
+        System.out.println("Umfang des Quadrats=" + umfang);
+        System.out.println("Umfang = " + umfang);
+        System.out.println("Zahl = " + zahl);
+    }
+}
