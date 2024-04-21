8b802d1498582f8bc69a857d34a794c5ac34e729
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-21 13:33:04 +0200

    ein Beispiel

---
+++ src/Tag03/_01_Frage.java
@@ -0,0 +1,14 @@
+package Tag03;
+
+public class _01_Frage {
+    public static void main(String[] args) {
+        int spenden1 = 500, spenden2 = 1000;
+        int summe = spenden1 + spenden2;
+        System.out.println("Summe der Spenden = " + summe + " €");
+
+        double gewicht = 74.2, grosse = 1.70;
+        double bmi = gewicht / (grosse * grosse);
+        System.out.println("Ihr Gewicht = " + gewicht + " und Ihre Größe = " + grosse);
+        System.out.println("Body-Mass-Index = " + bmi);
+    }
+}
