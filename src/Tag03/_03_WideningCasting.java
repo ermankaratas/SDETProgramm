f6c04ba1d1692efd5a6b50c61888d036d92513d7
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-23 20:56:36 +0200

    Widening Casting

---
+++ src/Tag03/_03_WideningCasting.java
@@ -0,0 +1,18 @@
+package Tag03;
+
+public class _03_WideningCasting {
+    public static void main(String[] args) {
+        int zahl = 9;
+        long summe = 500;
+        double rate = 3.2;
+
+        rate = zahl; // int -> double Richtung der Erweiterung, keine Möglichkeit für Datenverlust
+        System.out.println("rate = " + rate);
+
+        summe = zahl;
+        System.out.println("summe = " + summe);
+
+        rate = summe;
+        System.out.println("rate = " + rate);
+    }
+}
