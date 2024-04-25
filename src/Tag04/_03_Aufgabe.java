152097ee7e408e28baa6b4bc681f706d2be8e4e0
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-25 18:21:18 +0200

    Aufgabe

---
+++ src/Tag04/_03_Aufgabe.java
@@ -0,0 +1,78 @@
+package Tag04;
+
+public class _03_Aufgabe {
+    public static void main(String[] args) {
+        // Erstellen Sie ein Boolean mit dem Wert 'true'. Drucken Sie das Boolean.
+        boolean i = true;
+        System.out.println("i = " + i);
+
+        // Erstellen Sie ein Double mit dem Wert 10.01. Drucken Sie das Double.
+        double iDouble = 10.01;
+        System.out.println("iDouble = " + iDouble);
+
+        // Erstellen Sie ein Boolean mit dem Wert 'false'. Drucken Sie das Boolean.
+        boolean f = false;
+        System.out.println("f = " + f);
+
+        // Erstellen Sie ein Byte mit dem Wert 5. Drucken Sie das Byte.
+        byte iByte = 5;
+        System.out.println("iByte = " + iByte);
+
+        // Erstellen Sie ein Byte mit dem Wert -4. Drucken Sie das Byte.
+        byte iByte2 = -4;
+        System.out.println("iByte2 = " + iByte2);
+
+        // Erstellen Sie ein Char mit dem Wert 'a' und drucken Sie es.
+        // Erstellen Sie ein Char mit dem Wert 'z' und drucken Sie es.
+        // Erstellen Sie ein Char mit dem Wert '&' und drucken Sie es.
+        char a = 'a';
+        char z = 'z';
+        char x = '&';
+        System.out.println("a = " + a);
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+
+        // Erstellen Sie ein Double mit dem Wert 120 und drucken Sie es.
+        // Erstellen Sie ein Double mit dem Wert 120.005 und drucken Sie es.
+        // Erstellen Sie ein Double mit dem Wert 550.24 und drucken Sie es.
+        double iDouble1 = 120;
+        double iDouble2 = 120.005;
+        double iDouble3 = 550.24;
+        System.out.println("iDouble1 = " + iDouble1);
+        System.out.println("iDouble2 = " + iDouble2);
+        System.out.println("iDouble3 = " + iDouble3);
+
+        // Erstellen Sie ein Float mit dem Wert 121.005f und drucken Sie es.
+        // Erstellen Sie ein Float mit dem Wert 123.3365f und drucken Sie es.
+        // Erstellen Sie ein Float mit dem Wert 3.55f und drucken Sie es.
+        float float1 = 121.005f;
+        float float2 = 123.3365f;
+        float float3 = 3.55f;
+        System.out.println("float1 = " + float1);
+        System.out.println("float2 = " + float2);
+        System.out.println("float3 = " + float3);
+
+        // Erstellen Sie ein Integer mit dem Wert 15 und drucken Sie es.
+        // Erstellen Sie ein Integer mit dem Wert 100 und drucken Sie es.
+        // Erstellen Sie ein Integer mit dem Wert 120 und drucken Sie es.
+        // Erstellen Sie ein Integer mit dem Wert -6500 und drucken Sie es.
+        int i2 = 15;
+        int i3 = 100;
+        int i4 = 120;
+        int i5 = -6500;
+        System.out.println("i2 = " + i2);
+        System.out.println("i3 = " + i3);
+        System.out.println("i4 = " + i4);
+        System.out.println("i5 = " + i5);
+
+        // Erstellen Sie ein Short mit dem Wert 12 und drucken Sie es.
+        // Erstellen Sie ein Short mit dem Wert 23 und drucken Sie es.
+        // Erstellen Sie ein Short mit dem Wert -100 und drucken Sie es.
+        short sh1 = 12;
+        short sh2 = 23;
+        short sh3 = -100;
+        System.out.println("sh1 = " + sh1);
+        System.out.println("sh2 = " + sh2);
+        System.out.println("sh3 = " + sh3);
+    }
+}
