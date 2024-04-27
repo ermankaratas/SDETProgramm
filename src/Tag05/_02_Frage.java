8da3980c752db04a0d33457ad46d527555c758ef
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-27 17:48:50 +0200

    Beispiel

---
+++ src/Tag05/_02_Frage.java
@@ -0,0 +1,13 @@
+package Tag05;
+
+import java.util.Scanner;
+
+public class _02_Frage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geben Sie Ihr Gewicht ein = ");
+        double gewicht = scanner.nextDouble();
+
+        System.out.println("Gewicht = " + gewicht);
+    }
+}
