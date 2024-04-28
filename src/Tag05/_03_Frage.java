1cd98adcf6954b74d06562bfd2965f35a10098ab
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-28 11:31:07 +0200

    Beispiel

---
+++ src/Tag05/_03_Frage.java
@@ -0,0 +1,16 @@
+package Tag05;
+
+import java.util.Scanner;
+
+public class _03_Frage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geben Sie Ihr Gewicht ein = ");
+        double gewicht = scanner.nextDouble();
+        System.out.print("Geben Sie Ihre Größe ein = ");
+        double groesse = scanner.nextDouble();
+
+        System.out.println("Ihre Größe beträgt " + groesse + " und Ihr Gewicht beträgt " + gewicht);
+        System.out.println("Ihr Body-Mass-Index beträgt " + (gewicht / (groesse * groesse)));
+    }
+}
