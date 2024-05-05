2310398064a23cc8f8f79a7f6ec6f2b84f9f3239
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-05-05 03:44:10 +0200

    StringOperations

---
+++ _01_JavaKurs/src/Tag05_StringOperations/_01_StringLength.java
@@ -0,0 +1,11 @@
+package Tag05_StringOperations;
+
+public class _01_StringLength {
+    public static void main(String[] args) {
+        String x = "Es regnet heute.";
+
+        int laenge = x.length();
+
+        System.out.println("Länge = " + laenge);
+    }
+}
