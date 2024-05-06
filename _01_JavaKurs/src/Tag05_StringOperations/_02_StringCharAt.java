9750ea2e029b027070bb28cf2dbbc3f6a6c957d7
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-05-06 14:31:30 +0200

    StringOperations

---
+++ _01_JavaKurs/src/Tag05_StringOperations/_02_StringCharAt.java
@@ -0,0 +1,11 @@
+package Tag05_StringOperations;
+
+public class _02_StringCharAt {
+    public static void main(String[] args) {
+        String x = "Es regnet heute.";
+
+        char ersteChar = x.charAt(0);
+
+        System.out.println("Der erste Buchstabe des Satzes ist " + ersteChar );
+    }
+}
