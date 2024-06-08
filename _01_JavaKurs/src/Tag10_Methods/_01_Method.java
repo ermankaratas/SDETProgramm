bddcd243acc39ebc06ddf4db3641becfcce5ac8d
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-06-08 17:16:12 +0200

    Methoden

---
+++ _01_JavaKurs/src/Tag10_Methods/_01_Method.java
@@ -0,0 +1,21 @@
+package Tag10_Methods;
+
+public class _01_Method {
+    // Funktion oder Methode, beides dasselbe
+
+    // Hier kann geschrieben werden
+
+    public static void main(String[] args) {
+
+        // Hier kann nicht geschrieben werden
+
+        arrayFuellen();
+
+    }
+
+    // Hier kann geschrieben werden
+
+    public static void arrayFuellen(){
+        System.out.println("Hallo Welt");
+    }
+}
